c3288f1cecf8ee4f62736f47382f1fc417616943
luizpoderoso <luizpoderoso72@outlook.com>
2025-02-18 23:29:16 -0300

    Tá no ar!

---
+++ src/main/java/com/lpdrs/decola_tech_dio/DecolaTechDioApplication.java
@@ -0,0 +1,13 @@
+package com.lpdrs.decola_tech_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTechDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTechDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lpdrs/decola_tech_dio/DecolaTechDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpdrs.decola_tech_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTechDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
